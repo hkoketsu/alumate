55665b1821cb92c844b2eaf10e0555545a7db3bd
vikas <vikassrinitb@gmail.com>
2020-06-18 12:42:46 +0530

    fix bugs on merge

--- backend/alumate_api/urls.py
+++ backend/alumate_api/urls.py
@@ -29,8 +29,5 @@ urlpatterns = [
     path('api/account/', include('account.urls', namespace='account')),
     path('api/post/', include('post.urls', namespace='post')),
     path('api/message/', include('message.urls', namespace='message')),
-<<<<<<< HEAD
     path('api/notification/', include('notification.urls', namespace='notification')),
-=======
->>>>>>> master
 ]
--- backend/message/views.py
+++ backend/message/views.py
@@ -5,10 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import serializers
 from . models import Message
-<<<<<<< HEAD
 from notification.models import Notification
-=======
->>>>>>> master
 from rest_framework import permissions
 import datetime
 from itertools import chain
@@ -26,7 +23,6 @@ class MessagesListView(generics.ListAPIView):
         user = self.request.user
         sender_list = Message.objects.filter(receiver=user).values_list('sender', flat=True)
         receiver_list = Message.objects.filter(sender=user).values_list('receiver', flat=True)
-<<<<<<< HEAD
         # Change notification for unread_messages_count to 0 if exist
         try:
             notification = Notification.objects.get(
@@ -40,9 +36,6 @@ class MessagesListView(generics.ListAPIView):
             notification.notification_data = 0
         notification.save()
         
-=======
-
->>>>>>> master
         received_message_list = []
         for sender in sender_list:
             latest_message = Message.objects.filter(sender=sender)[0]
@@ -76,15 +69,9 @@ class MessageListView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         user = self.request.user
-<<<<<<< HEAD
         other_user = get_object_or_404(User, pk=self.kwargs.pop('pk'))
         received_messages = Message.objects.filter(sender=other_user, receiver=user)
         sent_message = Message.objects.filter(sender=user, receiver=other_user)
-=======
-        to_user = get_object_or_404(User, pk=self.kwargs.pop('pk'))
-        received_messages = Message.objects.filter(sender=to_user, receiver=user)
-        sent_message = Message.objects.filter(sender=user, receiver=to_user)
->>>>>>> master
         messages = sorted(chain(received_messages, sent_message), key=lambda x: x.sent_datetime)
         return messages
 
@@ -97,7 +84,6 @@ class MessageListView(generics.ListCreateAPIView):
                 receiver = other_user,
                 body = serializer.validated_data['body'],
             )
-<<<<<<< HEAD
         #  Add/upadate notification for the message sent
         try:
             notification = Notification.objects.get(
@@ -111,8 +97,6 @@ class MessageListView(generics.ListCreateAPIView):
                 notification_type='unread_messages_count',
                 notification_data = 1
             )
-=======
->>>>>>> master
         serializer = serializers.MessageSerializer(instance=message)
         headers = self.get_success_headers(serializer.data)
         return Response(data=serializer.data, status=status.HTTP_201_CREATED, headers=headers)

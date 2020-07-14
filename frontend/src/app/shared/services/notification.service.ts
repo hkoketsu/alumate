350d8f3d1704b8bf264bd8b533c9aa2cdcd462eb
vikas <vikassrinitb@gmail.com>
2020-07-14 22:35:04 +0530

    add notification service

---
+++ frontend/src/app/shared/services/notification.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TokenService } from '../../features/auth/services/token.service';
+import { ajax } from 'rxjs/ajax';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+    unread_messages_count : Number;
+
+    constructor(
+        private http: HttpClient,
+        @Inject('BASE_API_URL') private baseUrl: string,
+        private tokenService: TokenService,
+      ) { }
+
+    getNotifications () {
+        return this.http.get(`${this.baseUrl}/notification`)
+    }
+}
